3bb8f1d48e2b343ee304d2e3e2031082b81490e9
NkunzimanaE <nkunzimanaerne@gmail.com>
2023-06-15 21:23:53 +0300

    I am adding a python file to my repository

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Hello, World !")
 
 name = input("Wow !, Hello and Welcome.\n what is your name Sir/Mme :")
-print("Wow !, Hello and Welcome Mr ", name)
+print(f"Wow !, Hello and Welcome Mr {name}")
