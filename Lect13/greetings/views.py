6c89c07199798c77c855c09e3cb2e3a135f08901
NkunzimanaE <nkunzimanaerne@gmail.com>
2023-07-04 22:18:12 +0300

    addall newfiles

--- Lect13/greetings/views.py
+++ Lect13/greetings/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpRespomnse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    return HttpRespomnse("Thanks for surfing us out of all those websites around !")
